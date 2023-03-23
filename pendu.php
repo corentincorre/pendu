d6b0edd26ce45ad99cb6cd9f9f1f9745eb1afbdc
Corentin Corre <corentin.corre@hotmail.fr>
2023-03-23 16:38:43 +0100

    object version

--- pendu.php
+++ pendu.php
@@ -22,63 +22,66 @@ $mots = [
 'Ananas'
 ];
 
-function randomWord($list){
-    $rand = rand(0, count($list)- 1);
-    return $list[$rand];
-}
 
-function gameTurn($mot, $letters, $life){
-    $win = true;
-    for ($i = 0; $i < strlen($mot); $i++) {
-        $char = substr($mot, $i, 1);
-        if (in_array(strtoupper($char), $letters)){
-            print_r( strtoupper($char));
+class Pendu
+{
+    function play($mots){
+        $this->life = 10;
+        $this->mot = $this->randomWord($mots);
+        $this->letters = [];
+        $win = $this->gameTurn(true);
+        while ($this->life > 0 && !$win){
+            $win = $this->gameTurn(); 
+        }    
+    }
+    private function randomWord($list){
+        $rand = rand(0, count($list)- 1);
+        return $list[$rand];
+    }
+
+    private function gameTurn($initialize = false){
+        $win = true;
+        if(!$initialize){
+            $letter = $this->askLetter();
+            if (strpos(strtoupper($this->mot), strtoupper($letter))!==false){
+            $this->letters[] = $letter;
+            }
+            else {
+            $this->life--;
+            }
         }
-        else {
-        print_r('_');
-        $win = false;
+        
+        for ($i = 0; $i < strlen($this->mot); $i++) {
+            $char = substr($this->mot, $i, 1);
+            if (in_array(strtoupper($char), $this->letters)){
+                print_r( strtoupper($char));
+            }
+            else {
+            print_r('_');
+            $win = false;
+            }
         }
+        if(!$this->life){
+            print_r("\nperdu");
+        }
+        elseif($win){
+            print_r("\ngagné");
+        }
+        else{
+            print_r("\nil reste ".$this->life.' vie'.($this->life > 1 ? 's': ''));
+        }
+        return $win;
     }
-    if(!$life){
-        print_r("\nperdu");
+
+    private function askLetter(){
+        $letter = readline("\nEntrer une lettre: ");
+        if (strlen($letter)!=1){
+            $letter = askLetter();
+        }
+        return strtoupper($letter);
     }
-    elseif($win){
-        print_r("\ngagné");
-    }
-    else{
-        print_r("\nil reste ".$life.' vie'.($life > 1 ? 's': ''));
-    }
-    return $win;
-}
-function isLetterInWord($mot,$letter){
-    return str_contains(strtoupper($mot), strtoupper($letter));
-}
-function askLetter(){
-    $letter = readline("\nEntrer une lettre: ");
-    if (strlen($letter)!=1){
-        $letter = askLetter();
-    }
-    return strtoupper($letter);
 }
 
-function play($mots){
-    $life = 10;
-    $win = false;
-    $mot = randomWord($mots);
-    $letters = [];
-    gameTurn($mot, $letters,$life);
-    while ($life > 0 && !$win){
-       $letter = askLetter();
-       if (strpos(strtoupper($mot), strtoupper($letter))!==false){
-        $letters[] = $letter;
-       }
-       else {
-        $life--;
-       }
-       $win = gameTurn($mot, $letters,$life); 
+$pendu= new Pendu;
+$pendu->play($mots);
 
-    }
-    
-}
-
-play($mots);
